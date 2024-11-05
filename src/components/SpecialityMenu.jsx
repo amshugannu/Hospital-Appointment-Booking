8d12c5d3d6b45ab81fd9d1092c71c7daa0b51bae
amshugannu <120478234+amshugannu@users.noreply.github.com>
2024-11-06 00:16:26 +0530

    Update SpecialityMenu.jsx

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const SpecialityMenu = () => {
   return (
-    <div className='flex flex-col items-center gap-4 py-16 text-white bg-gray-800' id='speciality'>
+    <div className='flex flex-col items-center gap-4 py-16 text-white bg-#0a0d10' id='speciality'>
       <h1 className='text-3xl font-medium'>Find by Speciality</h1>
       <p className='sm:w-1/3 text-center text-gray-400'>Select from a variety of medical specialties.</p>
       <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
