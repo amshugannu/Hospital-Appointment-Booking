22000cc86a24d3b442413297ac3e226485df9f77
amshugannu <120478234+amshugannu@users.noreply.github.com>
2024-11-20 15:29:57 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
   }
   return (
     <form className='min-h-[80vh] flex items-center'>
-      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-sm shadow-lg'>
         <p className='text-2xl text-white font-semibold'>{state === 'Sign Up'? "Create Account":"Login"}</p>
         <p className='text-white'>Please {state==='Sign Up' ? "sign up" : "log in"} to book appointment</p>
         {
