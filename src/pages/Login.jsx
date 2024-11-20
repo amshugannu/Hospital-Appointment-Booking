4addc903aecef100a6555f0cd80d9d81caadeb7f
amshugannu <120478234+amshugannu@users.noreply.github.com>
2024-11-20 15:19:11 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,23 +10,23 @@ const Login = () => {
   return (
     <form className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
-        <p className='text-2xl font-semibold'>{state === 'Sign Up'? "Create Account":"Login"}</p>
-        <p>Please {state==='Sign Up' ? "sign up" : "log in"} to book appointment</p>
+        <p className='text-2xl text-white font-semibold'>{state === 'Sign Up'? "Create Account":"Login"}</p>
+        <p className='text-white'>Please {state==='Sign Up' ? "sign up" : "log in"} to book appointment</p>
         {
           state==="Sign Up" &&
           <div className='w-full'>
-          <p>Full Name</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-1'  type="text" value={name} onChange={(e)=>setName(e.target.value)} required/>
+          <p className='text-white'>Full Name</p>
+          <input className='border border-zinc-300 text-black rounded w-full p-2 mt-1'  type="text" value={name} onChange={(e)=>setName(e.target.value)} required/>
         </div>
         }
         
         <div className='w-full'>
-          <p>Email</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+          <p className='text-white'>Email</p>
+          <input className='border border-zinc-300 text-black rounded w-full p-2 mt-1' type="email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
         </div>
         <div className='w-full'>
-          <p>Password</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-1'  type="password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
+          <p className='text-white'>Password</p>
+          <input className='border border-zinc-300 text-black rounded w-full p-2 mt-1'  type="password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
         </div>
         <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up'? "Create Account":"Login"}</button>
         {
